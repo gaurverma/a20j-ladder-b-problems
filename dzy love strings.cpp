c280b84e100bc31cf724c1d2f61757867a29b103
gaurverma <55631123+gaurverma@users.noreply.github.com>
2020-05-12 09:42:41 +0530

    Add files via upload

---
+++ dzy love strings.cpp
@@ -0,0 +1,79 @@
+#include <bits/stdc++.h> // Include every standard library 
+#include <algorithm>
+using namespace std; 
+  
+typedef long long LL; 
+typedef pair<int, int> pii; 
+typedef pair<LL, LL> pll; 
+typedef pair<string, string> pss; 
+typedef vector<int> vi; 
+typedef vector<string> vs;
+typedef vector<vi> vvi; 
+typedef vector<pii> vii; 
+typedef vector<LL> vl; 
+typedef vector<vl> vvl; 
+  
+
+#ifdef TESTING 
+#define DEBUG fprintf(stderr, "====TESTING====\n") 
+#define VALUE(x) cerr << "The value of " << #x << " is " << x << endl 
+#define debug(...) fprintf(stderr, __VA_ARGS__) 
+#else 
+#define DEBUG 
+#define VALUE(x) 
+#define debug(...) 
+#endif 
+  
+#define FOR(a, b, c) for (int(a) = (b); (a) < (c); ++(a)) 
+#define FORN(a, b, c) for (int(a) = (b); (a) <= (c); ++(a)) 
+#define FORD(a, b, c) for (int(a) = (b); (a) >= (c); --(a)) 
+#define FORSQ(a, b, c) for (int(a) = (b); (a) * (a) <= (c); ++(a)) 
+#define FORC(a, b, c) for (char(a) = (b); (a) <= (c); ++(a)) 
+#define FOREACH(a, b) for (auto&(a) : (b)) 
+#define REP(i, n) FOR(i, 0, n) 
+#define REPN(i, n) FORN(i, 1, n) 
+#define MAX(a, b) a = max(a, b) 
+#define MIN(a, b) a = min(a, b) 
+#define SQR(x) ((LL)(x) * (x)) 
+#define RESET(a, b) memset(a, b, sizeof(a)) 
+#define fi first 
+#define se second 
+#define mp make_pair 
+#define pb push_back 
+#define ALL(v) v.begin(), v.end() 
+#define ALLA(arr, sz) arr, arr + sz 
+#define SIZE(v) (int)v.size() 
+#define SORT(v) sort(ALL(v)) 
+#define REVERSE(v) reverse(ALL(v)) 
+#define SORTA(arr, sz) sort(ALLA(arr, sz)) 
+#define REVERSEA(arr, sz) reverse(ALLA(arr, sz)) 
+#define PERMUTE next_permutation 
+#define TC(t) while (t--) 
+
+
+int main()
+{
+    LL arr[26],k,sum=0;
+    string str;
+    cin>>str;
+    cin>>k;
+    REP(i,26) cin>>arr[i];
+    for(int i=0;i<str.size();i++)
+    {
+    sum += (i+1)*arr[int(str[i])-97];
+    }
+    for(int i=str.size()+1;i<=str.size()+k;i++)
+    sum += i*(*max_element(arr,arr+26));
+    cout<<sum<<endl;
+    return 0;
+}
+    
+
+    
+    
+    
+    
+    
+    
+    
+    
